a13154600b4202297f4b1305bea3e3c0b31727ac
Eduardo Kairalla <eduardokairalla123@gmail.com>
2025-11-29 19:43:21 -0300

    add openai_client to dependency injection schema

--- python-api/opty_api/schemas/container.py
+++ python-api/opty_api/schemas/container.py
@@ -6,6 +6,7 @@ Dependency injection schemas.
 from opty_api.models import Config
 from opty_api.mongo.repositories.users import UserRepository
 from opty_api.mongo.setup.connection import MongoDBSetup
+from openai import AsyncOpenAI
 from supabase import AsyncClient
 from typing import TypedDict
 
@@ -19,3 +20,4 @@ class Container(TypedDict):
     supabase_client: AsyncClient
     mongodb: MongoDBSetup
     user_repository: UserRepository
+    openai_client: AsyncOpenAI
